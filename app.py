202f66005f1019f086215bfee55cf62777db57cb
Emilly Fernandes <emilly.fernandes0925@gmail.com>
2025-05-18 17:48:59 -0300

    gerando url encurtada e traçando de volta ao site

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import uuid
 
 from flask import Flask, request, render_template
 from extensions import db
+from markupsafe import escape
+from models.models import Link
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'database.db')
@@ -15,20 +17,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
-def getting_values():
+def form():
     if request.method == 'POST':
         original_url = request.form['url']
         if is_url(original_url):
+            '''
             link = create_link(original_url)
             add_link_database(link)
             return render_template("index.html", links = Link.query.all())
+            '''
+            return f'<p>IS A LINK</p>'
         else:
             return f'<p>IS NOT A LINK</p>'
     else:
-        return render_template("index.html")
+        return f'<a href="{generate_shorten_url(random_short_code())}" target="_blank">link</a>'
+        # return render_template("index.html")
 
 
-from models.models import Link
+@app.route('/redirect/<path:shortenurl>')
+def redirect(shortenurl):
+    return f'CODIGO: {escape(shortenurl)}'
+
+
+
+def generate_shorten_url(code):
+    PAGE_PATH = 'http://127.0.0.1:5000/redirect/'
+    shorten_url = PAGE_PATH + code
+    return shorten_url
+
 
 def add_link_database(link:Link):
     db.session.add(link)
@@ -53,7 +69,6 @@ def random_short_code():
 def is_url(string) -> bool:
     pattern = r'^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/\S*)?$'
     return re.match(pattern, string) is not None
-
 
 
 with app.app_context():
