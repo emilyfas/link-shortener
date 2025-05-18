5351ea9c92de6db4d014f5a2d8f2385729507bbe
Emilly Fernandes <emilly.fernandes0925@gmail.com>
2025-05-18 16:27:16 -0300

    recebendo e validando o url do form

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import re
 
-from flask import Flask
+from flask import Flask, request, render_template
 from extensions import db
 app = Flask(__name__)
 
@@ -9,7 +10,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+@app.route('/', methods=['GET', 'POST'])
+def getting_values():
+    if request.method == 'POST':
+        original_url = request.form['url']
+        if is_url(original_url):
+            return f'<p>VALID LINK</p>'
+        else:
+            return f'<p>IS NOT A LINK</p>'
+    else:
+        return render_template("index.html")
+
+
+def is_url(string) -> bool:
+    pattern = r'^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/\S*)?$'
+    return re.match(pattern, string) is not None
+
+
 from models.models import Link
 
 with app.app_context():
     db.create_all()
+
+
+
+
+
+
+
+
+
+
+
