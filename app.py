cc4a050b5e66d759267e6add69b1a1a7aada5016
Emilly Fernandes <emilly.fernandes0925@gmail.com>
2025-05-18 15:48:55 -0300

    criando estrutura do banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+import os
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
+from extensions import db
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+from models.models import Link
+
+with app.app_context():
+    db.create_all()
--- models/models.py
+++ models/models.py
@@ -1,1 +1,7 @@
+from extensions import db
 
+class Link:
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_code = db.Column(db.String(10), unique=True, nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False)
